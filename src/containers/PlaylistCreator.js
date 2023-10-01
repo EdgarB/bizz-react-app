32faa7af4cc7468d972525711e759ded0db5b444
edgarb <edgar_daniel_1996@hotmail.com>
2023-10-01 12:54:58 -0600

    Add ability to change playlist name

--- src/containers/PlaylistCreator.js
+++ src/containers/PlaylistCreator.js
@@ -1,45 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Card from '../components/Card';
 import SongList from '../components/SongList';
 import './PlaylistCreator.scss';
 import Button from '../components/Button';
 import {createPlaylistOnSpotify} from '../util/SpotiftyApi';
+import InputText from '../components/InputText';
 
 function PlaylistCreator(props){
-  function songsSelectedURIS(){
+  const [playlistName, setPlaylistName] = useState('THE PLAYLIST');
+
+  const playlistNameInputChangeHandler = (event)=>{
+    setPlaylistName(event.currentTarget.value);
+  }
+
+  const isDataReadyToCreatePlaylist = ()=>{
+    return playlistName.trim().length > 0 && props.songs.length > 0;
+  }
+
+  const onClickSaveHandler = (event) => {
+    if(isDataReadyToCreatePlaylist()){
+      createPlaylistOnSpotify( playlistName,songsSelectedURIS());
+    }
+  }
+
+  const songsSelectedURIS = ()=>{
     return props.songs.map((s)=>{
       return s.uri;
     })
-  }
-
-  function Header(){
-    return(
-      <h1> Playlist </h1>
-    )
-  }
-
-  function Body(){
-    return(
-      <SongList 
-          handleOnClickAdd={props.handleOnClickAdd}
-          handleOnClickRemove={props.handleOnClickRemove}
-          songsSelected={props.songsSelected}
-          songs={props.songs || []}/>
-    )
-  }
-
-  function Footer(){
-    return(
-      <Button onClick={()=>{ createPlaylistOnSpotify('THE PLAYLIST 2',songsSelectedURIS())}} type='secondary'>Save to spotify</Button>
-    )
   }
   
   return(
     <div className='c-playlist-creator'>
       <Card
-        header= {<Header/>}
-        body={<Body/>}
-        footer={<Footer/>}
+        header= {<div>
+          <h1 className='h-mb-8px'>Playlist</h1>
+          <InputText
+          value={playlistName}
+          name='playlist_name'
+          placeholder='Playlist name'
+          
+          onChange={playlistNameInputChangeHandler}/>
+        </div>}
+        body={<SongList 
+          handleOnClickAdd={props.handleOnClickAdd}
+          handleOnClickRemove={props.handleOnClickRemove}
+          songsSelected={props.songsSelected}
+          songs={props.songs || []}/>}
+        footer={<Button 
+          isDisabled={!isDataReadyToCreatePlaylist()} 
+          onClick={onClickSaveHandler} 
+          type='secondary'>Save to spotify</Button>}
       />
     
     </div>
